4b6ebb33d5c92054d5de973c7ac8f8d9449870d3
Mathisha Karunaratne <mkarunaratne@sandiego.edu>
2022-07-04 11:50:16 +0530

    added post and delete

--- src/main.py
+++ src/main.py
@@ -6,17 +6,26 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
+    description: Union[str, None] = None
     price: float
-    is_offer: Union[bool, None] = None
+    tax: Union[float, None] = None
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/items/{item_id}")
+async def add_item(item: Item):  
+    return item
+
+@app.delete("/items/{item_id}")
+async def del_item(item: Item):
+    return item
